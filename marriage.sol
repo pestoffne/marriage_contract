fabf5720faa39a9a50cf51aafe0ffb245f4f4660
Evgeny Pestov <pestoffne@vtomske.ru>
2022-10-09 16:43:22 +0700

    Update marriage.sol

--- marriage.sol
+++ marriage.sol
@@ -2,37 +2,47 @@
 pragma solidity ^0.8.17;
 
 struct Couple {
-    address husband;
-    address wife;
-    //bytes registration_number;
+    address spouse1;
+    address spouse2;
 }
+
 
 contract Marriage {
     address payable public root;
-    mapping (address /*proposed*/ => address /*proposer*/) proposed_marriages;
+
+    mapping (address /*proposed*/ => address /*proposer*/) _proposed_marriages;
     mapping (address /*spouse*/ => Couple) public married_couples;
 
     constructor() {
         root = payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
     }
 
-    function root_destruct() public {
-        require(msg.sender == root);
+    modifier rootOnly() {
+        require(msg.sender == root, "Only root can call this.");
+        _;
+    }
+
+    function rootDestruct() public rootOnly {
         selfdestruct(root);
     }
 
-    function generate_registration_number(address husband, address wife) public pure returns (uint160) {
-        return uint160(husband) / 2 + uint160(wife) / 2;
+    function generateRegistrationNumber(address spouse1, address spouse2) public pure returns (uint160) {
+        return uint160(spouse1) / 2 + uint160(spouse2) / 2;
     }
 
-    function propose_marriage(address proposed) public {
-        proposed_marriages[proposed] = msg.sender;
-        //emit 
+    event proposedMarriage(address proposer, address proposed);
+    event acceptedMarriage(address proposer, address proposed);
+
+    function proposeMarriage(address proposed) public {
+        require(_proposed_marriages[msg.sender] != proposed, "Internal marriage propose already exists.");
+        //require(married_couples[msg.sender] ..., "Couple is already married.");
+        _proposed_marriages[proposed] = msg.sender;
+        emit proposedMarriage(msg.sender, proposed);
     }
 
-    function accept_marriage(address proposer) public {
-        require(proposed_marriages[msg.sender] == proposer);
-        married_couples[msg.sender] = married_couples[proposer] = Couple({husband: proposer, wife: msg.sender});
-        //emit
+    function acceptMarriage(address proposer) public {
+        require(_proposed_marriages[msg.sender] == proposer, "Specified marriage propose is not found.");
+        married_couples[msg.sender] = married_couples[proposer] = Couple({spouse1: proposer, spouse2: msg.sender});
+        emit acceptedMarriage(proposer, msg.sender);
     }
 }
