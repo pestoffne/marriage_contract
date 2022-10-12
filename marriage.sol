f92e31248af6d59bd80d5e6431b2f6acc00574f9
Evgeny Pestov <pestoffne@vtomske.ru>
2022-10-12 21:59:51 +0700

    Update marriage.sol

--- marriage.sol
+++ marriage.sol
@@ -1,48 +1,84 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.17;
 
+abstract contract OwnerDestructable {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+
+    function destruct() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    function transferOwnership(address payable newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
+
+
 struct Couple {
     address spouse1;
     address spouse2;
 }
 
 
-contract Marriage {
-    address payable public root;
+function contains(Couple storage couple, address spouse) view returns (bool) {
+    return couple.spouse1 == spouse || couple.spouse2 == spouse;
+}
 
-    mapping (address /*proposed*/ => address /*proposer*/) _proposed_marriages;
-    mapping (address /*spouse*/ => Couple) public married_couples;
 
-    constructor() {
-        root = payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
-    }
+function generateRegistrationNumber(address spouse1, address spouse2) pure returns (uint160) {
+    return uint160(spouse1) / 2 + uint160(spouse2) / 2;
+}
 
-    modifier rootOnly() {
-        require(msg.sender == root, "Only root can call this.");
-        _;
-    }
 
-    function rootDestruct() public rootOnly {
-        selfdestruct(root);
-    }
-
-    function generateRegistrationNumber(address spouse1, address spouse2) public pure returns (uint160) {
-        return uint160(spouse1) / 2 + uint160(spouse2) / 2;
-    }
+contract Marriage is OwnerDestructable {
+    mapping (address /*proposed keccac256*/ => address[] /*proposers*/) private _proposed_marriages;
+    mapping (address /*spouse keccac256*/ => Couple /*spouse1, spouse2*/) public married_couples;
 
     event proposedMarriage(address proposer, address proposed);
     event acceptedMarriage(address proposer, address proposed);
+    //event rejectMarriage(address proposer, address proposed);
 
     function proposeMarriage(address proposed) public {
-        require(_proposed_marriages[msg.sender] != proposed, "Internal marriage propose already exists.");
-        //require(married_couples[msg.sender] ..., "Couple is already married.");
-        _proposed_marriages[proposed] = msg.sender;
+        require(!verifyAddressIsMarried(proposed), "Specified address is already married.");
+        require(!verifyAddressIsMarried(msg.sender), "Your address is already married.");
+        require(!_marriageProposeExists(proposed, msg.sender), "Such marriage propose already exists.");
+        require(!_marriageProposeExists(msg.sender, proposed), "There is incoming marriage propose from specified address. Accept it instead.");
+        _proposed_marriages[proposed].push(msg.sender);
         emit proposedMarriage(msg.sender, proposed);
     }
 
     function acceptMarriage(address proposer) public {
-        require(_proposed_marriages[msg.sender] == proposer, "Specified marriage propose is not found.");
+        require(!verifyAddressIsMarried(proposer), "Specified address is already married.");
+        require(!verifyAddressIsMarried(msg.sender), "Your address is already married.");
+        require(_marriageProposeExists(proposer, msg.sender), "Marriage propose from specified address is not found.");
         married_couples[msg.sender] = married_couples[proposer] = Couple({spouse1: proposer, spouse2: msg.sender});
         emit acceptedMarriage(proposer, msg.sender);
+    }
+
+    function verifyCoupleIsMarried(address address1, address address2) public view returns (bool) {
+        return contains(married_couples[address1], address2);
+    }
+
+    function verifyAddressIsMarried(address address1) public view returns (bool) {
+        return married_couples[address1].spouse1 != address(0);
+    }
+
+    function _marriageProposeExists(address proposer, address proposed) private view returns (bool) {
+        for (uint8 i = 0 ; i < _proposed_marriages[proposed].length; i++) {
+            if (_proposed_marriages[proposed][i] == proposer) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
